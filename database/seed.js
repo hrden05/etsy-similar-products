a10472a5ccce83e386d2e7f0e67a5a4f37e4ba74
Lisa White <whitelisab@gmail.com>
2020-06-18 19:49:30 -0600

    Create seed.js and script
    Successfully adds store and product data to db. Tested with api calls
    to db for both collections.

--- database/seed.js
+++ database/seed.js
@@ -1,16 +1,23 @@
-const db = require('./index.js');
+const mongoose = require('mongoose');
 const faker = require('faker');
+const db = require('./index.js');
 
 // seeding script
 
 // drop database first
-// db.dropDatabase();
+// db.dropDatabase((err, result) => {
+//   if (err) {
+//     console.log('error dropping db');
+//   } else {
+//     console.log('dropped db');
+//   }
+// });
 
-// create 25 stores, add to array, use insertmany
+// create 25 stores
 const stores = [];
 
-for (let i = 1; i <= 25; i++) {
-  let store = new db.Store({
+for (let i = 1; i <= 25; i += 1) {
+  const store = new db.Store({
     _id: new mongoose.Types.ObjectId(),
     name: faker.lorem.word(),
     store_id: i,
@@ -23,13 +30,15 @@ for (let i = 1; i <= 25; i++) {
   stores.push(store);
 }
 
+console.log(`populating db with ${stores.length} stores `);
+
 // create 100 products
 const products = [];
 const shippingOptions = ['FREE shipping', 'Free shipping eligible', ''];
 const productCategories = ['clothing', 'art', 'jewelry', 'misc'];
 
-for (let i = 1; i <= 100; i++) {
-  let productCateory;
+for (let i = 1; i <= 100; i += 1) {
+  let productCategory;
   if (i <= 25) {
     productCategory = productCategories[0];
   } else if (i <= 50) {
@@ -40,75 +49,40 @@ for (let i = 1; i <= 100; i++) {
     productCategory = productCategories[3];
   }
 
-  let product = new db.Product({
+  const product = new db.Product({
     _id: new mongoose.Types.ObjectId(),
     name: faker.commerce.productName(),
     product_id: i,
     price: faker.commerce.price(5.00, 250.00, 2),
-    image: faker.image.imageUrl(),
-    shipping: shippingOptions[faker.random.number({ min: 0, max: 2})],
+    image: faker.image.image(),
+    shipping: shippingOptions[faker.random.number({ min: 0, max: 2 })],
     sponsored: faker.random.boolean(),
     category: productCategory,
-    store_id: faker.random.number({ min: 1, max: 25}),
-    store: sampleStore._id,
+    store_id: faker.random.number({ min: 1, max: 25 }),
+    // store: stores[faker.random.number({ min: 1, max: 25 })][_id],
   });
   products.push(product);
 }
 
+console.log(`populating db with ${products.length} products `);
 
-const store = {
-  _id: new mongoose.Types.ObjectId(),
-  name: 'Etsy Store',
-  store_id: 200,
-  logo: 'https://i.etsystatic.com/isla/362517/41194900/isla_140x140.41194900_5nj8y8kp.jpg?version=0',
-  items_count: 50,
-  sales_count: 900,
-  start_year: 2010,
-  location: 'New York, NY',
+// iterate over both the stores and products arrays to add each item to the db
+stores.forEach((store) => {
+  store.save((err, result) => {
+    if (err) {
+      console.log('error adding store to db');
+    } else {
+      console.log(`saved store ${store.store_id} in db`);
+    }
+  });
 });
 
-sampleStore.save((err, result) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('sample store saved', result);
-
-    const product = {
-      _id: new mongoose.Types.ObjectId(),
-      name: 'Fine Art. Cactus',
-      product_id: 550,
-      price: 75.00,
-      image: 'https://i.etsystatic.com/6507355/r/il/58ee24/905514140/il_1140xN.905514140_17k6.jpg',
-      shipping: 'FREE Shipping',
-      sponsored: false,
-      category: 'Art',
-      store_id: 200,
-      store: sampleStore._id,
-    });
-
-    sampleProduct.save((error, results) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log('sample product saved', results);
-      }
-    });
-  }
-});
-
-
-// set up api to test faker
-app.get('/api', function(req, res) {
-  res.json([
-      {
-          title: faker.lorem.words(),
-          author: faker.name.findName(),
-          author_image: faker.image.avatar(),
-          release_date: faker.date.recent(),
-          image: faker.image.abstract(),
-          price: faker.commerce.price(),
-          short_description: faker.lorem.sentence(),
-          long_description: faker.lorem.paragraph()
-      }
-  ])
+products.forEach((product) => {
+  product.save((err, result) => {
+    if (err) {
+      console.log('error adding product to db');
+    } else {
+      console.log(`saved product ${product.product_id} in db`);
+    }
+  });
 });
--- server/index.js
+++ server/index.js
@@ -12,32 +12,67 @@ app.listen(PORT, () => {
   console.log(`listening on PORT ${PORT}`);
 });
 
+// tests for getting data in db
 app.get('/api/stores', (req, res) => {
-  res.json([
-    {
-      _id: new mongoose.Types.ObjectId(),
-      name: faker.lorem.word(),
-      store_id: 10,
-      logo: faker.image.abstract(),
-      items_count: faker.random.number({ min: 10, max: 1000 }),
-      sales_count: faker.random.number({ min: 100, max: 10000 }),
-      start_year: faker.random.number({ min: 2005, max: 2020 }),
-      location: `${faker.address.city()}, ${faker.address.stateAbbr()}`,
-    },
-  ]);
+  let query = db.Store.find();
+  query.exec((err, results) => {
+    if (err) {
+      console.log('error getting all stores from db');
+    } else {
+      console.log('got stores from db');
+      res.json({
+        stores: results,
+      });
+    }
+  });
 });
 
 app.get('/api/products', (req, res) => {
-  res.json([
-    {
-      _id: new mongoose.Types.ObjectId(),
-      name: faker.company.companyName(),
-      store_id: 10,
-      logo: faker.image.abstract(),
-      items_count: faker.random.number({ min: 10, max: 1000 }),
-      sales_count: faker.random.number({ min: 100, max: 10000 }),
-      start_year: faker.random.number({ min: 2005, max: 2020 }),
-      location: `${faker.address.city()}, ${faker.address.stateAbbr()}`,
-    },
-  ]);
+  let query = db.Product.find();
+  query.exec((err, results) => {
+    if (err) {
+      console.log('error getting all products from db');
+    } else {
+      console.log('got products from db');
+      res.json({
+        stores: results,
+      });
+    }
+  });
 });
+
+// tests for faker data
+// app.get('/api/stores', (req, res) => {
+//   res.json([
+//     {
+//       _id: new mongoose.Types.ObjectId(),
+//       name: faker.lorem.word(),
+//       store_id: 10,
+//       logo: faker.image.abstract(),
+//       items_count: faker.random.number({ min: 10, max: 1000 }),
+//       sales_count: faker.random.number({ min: 100, max: 10000 }),
+//       start_year: faker.random.number({ min: 2005, max: 2020 }),
+//       location: `${faker.address.city()}, ${faker.address.stateAbbr()}`,
+//     },
+//   ]);
+// });
+
+// app.get('/api/products', (req, res) => {
+//   const shippingOptions = ['FREE shipping', 'Free shipping eligible', ''];
+//   const productCategories = ['clothing', 'art', 'jewelry', 'misc'];
+
+//   res.json([
+//     {
+//       _id: new mongoose.Types.ObjectId(),
+//       name: faker.commerce.productName(),
+//       product_id: 15,
+//       price: faker.commerce.price(5.00, 250.00, 2),
+//       image: faker.image.image(),
+//       shipping: shippingOptions[faker.random.number({ min: 0, max: 2 })],
+//       sponsored: faker.random.boolean(),
+//       category: productCategories[faker.random.number({ min: 0, max: 3 })],
+//       store_id: faker.random.number({ min: 1, max: 25 }),
+//       store: new mongoose.Types.ObjectId(),
+//     },
+//   ]);
+// });
